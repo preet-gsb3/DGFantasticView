5f14ad78e122038f2c4fc7316035261f4ae9c10b
Gurpreet Singh <preet.gsb3@gmail.com>
2020-02-08 01:06:09 +0530

    Initial Commit

---
+++ DGFantastic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DGFantastic
+//
+//  Created by Gurpreet Singh on 08/02/20.
+//  Copyright © 2020 Gurpreet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
